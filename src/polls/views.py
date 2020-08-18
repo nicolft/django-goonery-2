a2100425effc8f00a1d8f6c09f9c0c307708f959
Salt <nicolft@gmail.com>
2020-08-18 14:20:17 -0500

    made views/urls dry
    for index and detail particularly

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,26 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+#from django.http import HttpResponse
+#from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    #template = loader.get_template('polls/index.html')
     context = { #this is a dictionarp
     	'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." %question_id)
+	q = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'q':q})
+
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id) #same thing as above
+	q = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'q':q})
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." %question_id)
+	try:
+		q = Question.objects.get(pk=1)
+	except:
+		raise Http404("Poll does not exist.")
+	context = {'q':q}
+	return render(request, 'polls/vote.html', context)
